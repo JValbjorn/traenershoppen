f176c0470a99b84304ab30eed632bc0b00a3dfbb
JValbjorn <julievalbjorn@live.dk>
2026-05-04 13:54:18 +0200

    feat: integrate Header component into Home page and update Header structure

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-var(--light_gray) font-sans dark:bg-var(--light-gray)">
+      <Header />
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-var(--light_gray) dark:var(--light-gray) sm:items-start">
         <Image
           className="dark:invert"
