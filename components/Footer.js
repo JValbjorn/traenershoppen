ea8d363218c40c20f9c9e9d82fcc7e4fe5468d7e
stinelock <eaa24stlo@students.eaaa.dk>
2026-05-06 09:35:18 +0200

    style: update Footer component layout for improved structure and alignment

--- components/Footer.js
+++ components/Footer.js
@@ -2,25 +2,24 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="w-full h-auto flex justify-center align-top bg-dark-green text-sm text-white p-4">
-      <div className="">
+    <footer className="w-full h-auto grid grid-cols-4 justify-center align-top bg-dark-green text-sm text-white p-4">
+      <div className="self-start justify-self-center">
         <p className="font-bold">Trænershoppen</p>
         <p>Vestergade 19b, 8000 Aarhus</p>
         <p>CVR: 1234568943</p>
       </div>
-      <div>
+      <div className="self-start justify-self-center">
         <p>Om os</p>
         <p>Leveringsbetingelser</p>
         <p>Inspiratoins side</p>
       </div>
-      <div>
+      <div className="self-start justify-self-center">
         <h4 className="font-bold">Telefontider</h4>
         <p>Mandag - Torsdag: 8 - 16</p>
         <p>Fredag: 8 - 15.30</p>
         <p>Weekend: Lukket</p>
       </div>
-      <Image/>
-      
+      <Image />
     </footer>
   );
 }
