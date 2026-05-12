703a39f0df60730b1c43463492134dbb22765770
JValbjorn <julievalbjorn@live.dk>
2026-05-12 11:33:32 +0200

    create productCard. test productCard on fodbold page

--- app/fodbold/page.js
+++ app/fodbold/page.js
@@ -1,8 +1,37 @@
+import ProductCard from "@/components/ProductCard";
+
 export default function fodbold() {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold mb-4">Fodbold</h1>
-            <p className="text-lg text-gray-600">Velkommen til fodbold siden!</p>      </div>
-    );
+    const products = [
+    {
+      id: "1",
+      name: "Select Pisa shorts - Sort",
+      brand: "SELECT",
+      price: 99.95,
+      oldPrice: 130,
+      onSale: true,
+      salePercent: 23,
+      image: "/img/shorts.png",
+    },
+    {
+      id: "2",
+      name: "Select Numero 10 V23 fodbold",
+      brand: "SELECT",
+      price: 899,
+      onSale: false,
+      image: "/img/ball.png",
+    },
+  ];
+
+  return (
+    <div className="p-8 bg-light_gray min-h-screen">
+      <h1 className="text-4xl font-bold mb-6">Fodbold</h1>
+
+      <div className="grid grid-cols-10 md:grid-cols-4 gap-6">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 
 }
