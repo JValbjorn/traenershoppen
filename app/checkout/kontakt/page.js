4da929a772ad39f5de9de9a7ea0e74b436ac4ec9
Mapou435 <eaa24mkrp@students.eaaa.dk>
2026-05-13 14:15:40 +0200

    Change to useeffect
    Ændret Localstorage til useEffect for at fixe deploy error fra vercel

--- app/checkout/kontakt/page.js
+++ app/checkout/kontakt/page.js
@@ -5,16 +5,33 @@ import CTAButton from "@/components/CTAButton";
 import CartPreview from "@/components/CartPreview";
 import { useRouter } from "next/navigation";
 import CheckoutNav from "@/components/CheckoutNav";
+import { useEffect, useState } from "react";
 
 export default function Kontakt() {
   const router = useRouter();
+  const [data, setData] = useState({
+    phone: "",
+    email: "",
+    firstName: "",
+    lastName: "",
+    address: "",
+    postalCode: "",
+    city: "",
+    country: "",
+    companyName: "",
+  });
+
+  useEffect(() => {
+      const savedData = JSON.parse(localStorage.getItem("checkoutContactInfo")) || {};
+      setData(savedData);
+  }, []);
+
   const handleChange = (e) => {
-    if (typeof window === "undefined") return;
     const { name, value } = e.target;
-    const currentData = JSON.parse(localStorage.getItem("checkoutContactInfo")) || {};
-    currentData[name] = value;
-    localStorage.setItem("checkoutContactInfo", JSON.stringify(currentData));
-    console.log("Data gemt", currentData);
+    const updatedData = { ...data, [name]: value };
+    setData(updatedData);
+    localStorage.setItem("checkoutContactInfo", JSON.stringify(updatedData));
+    console.log("Data gemt", updatedData);
   };
 
   return (
@@ -22,16 +39,25 @@ export default function Kontakt() {
       <div className="flex flex-row gap-10">
         <Link href="/produktoversigt">Tilbage</Link>
         <article>
-
-          {/* autoComplete er det so mgør google autofyld virker */}
-          <form
-            className="flex flex-col gap-4"
-          >
-          {/*CheckoutNav er navigation mellem steps */}
-          <CheckoutNav />
+          <form className="flex flex-col gap-4">
+            <CheckoutNav />
             <h3>Kontaktoplysninger</h3>
-            <input type="tel"  name="phone" placeholder="Telefonnummer" autoComplete="tel"  onChange={handleChange} defaultValue={JSON.parse(localStorage.getItem("checkoutContactInfo"))?.phone || ""}/>
-            <input type="email" name="email" placeholder="Email" autoComplete="email" onChange={handleChange} defaultValue={JSON.parse(localStorage.getItem("checkoutContactInfo"))?.email || ""}/>
+            <input
+              type="tel"
+              name="phone"
+              placeholder="Telefonnummer"
+              autoComplete="tel"
+              onChange={handleChange}
+              value={data.phone}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              autoComplete="email"
+              onChange={handleChange}
+              value={data.email}
+            />
             <h3>Leveringsadresse</h3>
             <div className="flex flex-row gap-4">
               <input
@@ -41,7 +67,7 @@ export default function Kontakt() {
                 autoComplete="given-name"
                 required
                 onChange={handleChange}
-                defaultValue={JSON.parse(localStorage.getItem("checkoutContactInfo"))?.firstName || ""}
+                value={data.firstName}
               />
               <input
                 type="text"
@@ -50,7 +76,7 @@ export default function Kontakt() {
                 autoComplete="family-name"
                 required
                 onChange={handleChange}
-                defaultValue={JSON.parse(localStorage.getItem("checkoutContactInfo"))?.lastName || ""}
+                value={data.lastName}
               />
             </div>
             <input
@@ -60,7 +86,7 @@ export default function Kontakt() {
               autoComplete="street-address"
               required
               onChange={handleChange}
-              defaultValue={JSON.parse(localStorage.getItem("checkoutContactInfo"))?.address || ""}
+              value={data.address}
             />
             <input
               type="text"
@@ -69,25 +95,25 @@ export default function Kontakt() {
               autoComplete="postal-code"
               required
               onChange={handleChange}
-              defaultValue={JSON.parse(localStorage.getItem("checkoutContactInfo"))?.postalCode || ""}
+              value={data.postalCode}
             />
             <input
               type="text"
               name="city"
               placeholder="By"
-              autoComplete=" locality"
-              onChange={handleChange}
+              autoComplete="locality"
               required
-              defaultValue={JSON.parse(localStorage.getItem("checkoutContactInfo"))?.city || ""}
+              onChange={handleChange}
+              value={data.city}
             />
             <input
               type="text"
               name="country"
               placeholder="Land"
               autoComplete="country"
-              onChange={handleChange}
               required
-              defaultValue={JSON.parse(localStorage.getItem("checkoutContactInfo"))?.country || ""}
+              onChange={handleChange}
+              value={data.country}
             />
             <input
               type="text"
@@ -95,23 +121,22 @@ export default function Kontakt() {
               placeholder="Firmanavn (valgfrit)"
               autoComplete="organization"
               onChange={handleChange}
-              defaultValue={JSON.parse(localStorage.getItem("checkoutContactInfo"))?.companyName || ""}
+              value={data.companyName}
             />
-          
-          <div className="flex flex-row justify-between mt-5">
-            <button
-              onClick={() => {
-                console.log("Shop videre er trykket");
-              }}
-              className="bg-none border-2 text-black font-bold min-w-40 px-6 py-3 hover:bg-black hover:text-white transition-colors duration-30"
-            >
-              Shop videre
-            </button>
-            <CTAButton
-              text="Gå til levering"
-              onClick={() => router.push("/checkout/levering", console.log("debug er trykket"))}
-            />
-          </div>
+            <div className="flex flex-row justify-between mt-5">
+              <button
+                onClick={() => {
+                  console.log("Shop videre er trykket");
+                }}
+                className="bg-none border-2 text-black font-bold min-w-40 px-6 py-3 hover:bg-black hover:text-white transition-colors duration-30"
+              >
+                Shop videre
+              </button>
+              <CTAButton
+                text="Gå til levering"
+                onClick={() => router.push("/checkout/levering", console.log("debug er trykket"))}
+              />
+            </div>
           </form>
         </article>
       </div>
