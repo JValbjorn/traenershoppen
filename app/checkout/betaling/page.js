b607f3003549287a4f5740aa37ccc98cc867a973
Mapou435 <eaa24mkrp@students.eaaa.dk>
2026-05-14 21:55:44 +0200

    Levering og Betaling checkout logik

--- app/checkout/betaling/page.js
+++ app/checkout/betaling/page.js
@@ -1,28 +1,44 @@
-"use client"; 
+"use client";
 
 import Link from "next/link";
 import CTAButton from "@/components/CTAButton";
 import CartPreview from "@/components/CartPreview";
 import CheckoutNav from "@/components/CheckoutNav";
+import OptionsCheckout from "@/components/OptionsCheckout";
+import { UserInfo } from "@/components/UserInfo";
 
 export default function Kontakt() {
-    return (
-        <div className="flex flex-row gap-4 px-30 py-20 justify-between"> 
-            <div className="flex flex-row gap-10">
-                <Link href="/checkout/levering">Tilbage</Link>
-                <article>
-                    {/*CheckoutNav er navigation mellem steps */}
-                    <CheckoutNav />
-                    <div className="flex flex-row justify-between mt-5">
-                        <button onClick={() => {console.log("Shop videre er trykket")}} className="bg-none border-2 text-black font-bold min-w-40 px-6 py-3 hover:bg-black hover:text-white transition-colors duration-30">Tilbage til levering</button>
-                        <CTAButton text="Bestil" onClick={() => {}} />
-                    </div>
-                </article>
-            </div>
-            <article>
-                <CartPreview/>
-            </article>
-        </div>
-    );
+  return (
+    <div className="flex flex-row gap-4 px-30 py-20 justify-between">
+      <div className="flex flex-row gap-10">
+        <Link href="/checkout/levering">Tilbage</Link>
+        <article>
+          {/*CheckoutNav er navigation mellem steps */}
+          <CheckoutNav />
+          <article>
+            <UserInfo />
 
+            <article className="mt-10">
+              <h3>Betaling</h3>
+              <OptionsCheckout />
+            </article>
+          </article>
+          <div className="flex flex-row justify-between mt-5">
+            <button
+              onClick={() => {
+                console.log("Shop videre er trykket");
+              }}
+              className="bg-none border-2 text-black font-bold min-w-40 px-6 py-3 hover:bg-black hover:text-white transition-colors duration-30"
+            >
+              Tilbage til levering
+            </button>
+            <CTAButton text="Bestil" onClick={() => {}} />
+          </div>
+        </article>
+      </div>
+      <article>
+        <CartPreview />
+      </article>
+    </div>
+  );
 }
--- app/checkout/levering/page.js
+++ app/checkout/levering/page.js
@@ -6,9 +6,14 @@ import { useRouter } from "next/navigation";
 import CartPreview from "@/components/CartPreview";
 import { UserInfo } from "@/components/UserInfo";
 import CheckoutNav from "@/components/CheckoutNav";
+import Image from "next/image";
+import Notification from "@/components/Notification";
+import { useState } from "react";
+import OptionsCheckout from "@/components/OptionsCheckout";
 
 export default function Kontakt() {
   const router = useRouter();
+  const [showNotification, setShowNotification] = useState(false);
 
   return (
     <div className="flex flex-row gap-4 px-30 py-20 justify-between">
@@ -19,29 +24,55 @@ export default function Kontakt() {
           <CheckoutNav />
           <div className="flex flex-col justify-between mt-5">
             <div>
-              {/* Her skal der laves en funktion der gemmer de indtastede oplysninger i en global state, så de kan bruges på betalings siden */}
+              {/* Userinfo displayer brugeren intastede data*/}
+              <div className="flex flex-row gap-2 items-center">
+                <Image
+                  src="/img/edit-button.png"
+                  alt="Description"
+                  width={15}
+                  height={15}
+                  onClick={() => {
+                    setShowNotification(true);
+                  }}
+                />
+              </div>
               <UserInfo />
+              <article className="mt-10">
+                <h3>Levering</h3>
+                <OptionsCheckout />
+              </article>
             </div>
             <div className="flex flex-row justify-between mt-5">
-            <button
-              onClick={() => {
-                console.log("Shop videre er trykket");
-              }}
-              className="bg-none border-2 text-black font-bold min-w-40 px-6 py-3 hover:bg-black hover:text-white transition-colors duration-30"
-            >
-              Tilbage til kontakt oplysninger
-            </button>
-            <CTAButton
-              text="Gå til betaling"
-              onClick={() => router.push("/checkout/betaling")}
-            />
+              <button
+                onClick={() => {
+                  console.log("Shop videre er trykket");
+                  router.push("/checkout/kontakt");
+                }}
+                className="bg-none border-2 text-black font-bold min-w-40 px-6 py-3 hover:bg-black hover:text-white transition-colors duration-30"
+              >
+                Tilbage til kontakt oplysninger
+              </button>
+              <CTAButton
+                text="Gå til betaling"
+                onClick={() => router.push("/checkout/betaling")}
+              />
             </div>
           </div>
         </article>
       </div>
+
       <article>
-        <CartPreview/>
+        <CartPreview />
       </article>
+
+      {/* Notification til at vise fejlmeddelelser */}
+      {showNotification && (
+        <Notification
+          text="Dine Oplysninger kan desværre ikke redigeres på nuværende tidspunkt, gå til Oplysninger for at ændre dem"
+          color="bg-red-500"
+          onClose={() => setShowNotification(false)}
+        />
+      )}
     </div>
   );
 }
